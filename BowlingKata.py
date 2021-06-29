a4407e3c42e85795ef801cc0496043fff510385a
pdawson1983 <pdawson1983@gmail.com>
2021-06-29 12:24:05 -0400

    Refactor code

--- BowlingKata.py
+++ BowlingKata.py
@@ -2,6 +2,7 @@ class Game():
     def __init__(self):
         self.rolls = []
         self.currentRoll = 0
+        self.tenPins = 10
 
     def roll(self, pins):
         self.rolls.append(pins)
@@ -10,25 +11,28 @@ class Game():
         score = 0
         frameIndex = 0
         for _ in range(10):
-            if self.isStrike(frameIndex):  # strike
-                score += 10 + self.strikeBonus(frameIndex)
+            if self.isStrike(frameIndex): 
+                score += self.tenPins + self.strikeBonus(frameIndex)
                 frameIndex += 1
             elif self.isSpare(frameIndex):
-                score += 10 + self.spareBonus(frameIndex)
+                score += self.tenPins + self.spareBonus(frameIndex)
                 frameIndex += 2
             else:
-                score += self.rolls[frameIndex] + self.rolls[frameIndex+1]
+                score += self.normalFrameScore(frameIndex)
                 frameIndex += 2
         return score
 
     def isStrike(self, frameIndex):
-        return self.rolls[frameIndex] == 10
+        return self.rolls[frameIndex] == self.tenPins
 
     def isSpare(self, frameIndex):
-        return self.rolls[frameIndex] + self.rolls[frameIndex + 1] == 10
+        return self.rolls[frameIndex] + self.rolls[frameIndex + 1] == self.tenPins
 
     def strikeBonus(self, frameIndex):
         return self.rolls[frameIndex+1] + self.rolls[frameIndex+2]
     
     def spareBonus(self, frameIndex):
         return self.rolls[frameIndex+2]
+    
+    def normalFrameScore(self, frameIndex):
+        return self.rolls[frameIndex] + self.rolls[frameIndex+1]
